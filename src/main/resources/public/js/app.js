fb40138fc6ca7e8b45ae9dcd3967cfa4a96c460b
Nikita Korobkov <nikkorobk@gmail.com>
2019-09-14 16:09:25 -0700

    minor ui improvements

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,12 +1,11 @@
 Vue.component('book-card', {
     props: ['book'],
-    template: `<div class="book">
-        <div class="book-body">
+    template: `
+        <div class="alert alert-secondary">
             <b class="book-title">
                 {{ book.title }}
             </b><p> with {{book.pages}} pages</p><br>
             <p>{{book.desc}}</p>
-        </div>
         </div>`
 });
 
@@ -15,14 +14,25 @@ var app = new Vue({
     el: '#contents',
     data: {
         query:"",
+        message:"",
+        error:"",
         books: []
     },
     methods:{
         getBooks: function () {
-            console.log('getBooksCalled');
-            this.$http.get('/searchByName', {params:{name: this.query}}).then((response) => {
-                this.books = response.data;
-            });
+            if (this.query){
+                this.message = "Looking for some books for you";
+                this.error = "";
+                this.$http.get('/searchByName', {params:{name: this.query}}).then((response) => {
+                    this.books = response.data;
+                    this.message = "";
+                    if (response.data.length === 0){
+                        this.error  = "No books found for your request, please try something else";
+                    }
+                });
+            } else {
+                this.error = "Please enter something in the search line"
+            }
         }
         
     }
